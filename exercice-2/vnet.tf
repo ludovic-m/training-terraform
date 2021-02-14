eb7f21095a97f595ef841ca982f776312fc4d704
Ludovic Medard <ludovic.medard.c@thalesdigital.io>
2021-02-14 09:11:56 +0100

    update 0.14

--- exercice-2/vnet.tf
+++ exercice-2/vnet.tf
@@ -9,5 +9,5 @@ resource "azurerm_subnet" "subnet_training" {
   name                 = "subnet_training"
   resource_group_name = azurerm_resource_group.rg_training.name
   virtual_network_name = azurerm_virtual_network.vnet_training.name
-  address_prefixes     = var.subnet_prefix
+  address_prefixes     = ["10.0.1.0/24"]
 }
