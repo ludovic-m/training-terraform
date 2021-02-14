6082a3df3aa7192c9d79a3ee7b6c89cc755c6ee5
Ludovic Medard <ludovic.medard.c@thalesdigital.io>
2021-02-14 17:39:03 +0100

    update 0.14

--- exercice-6/main.tf
+++ exercice-6/main.tf
@@ -5,7 +5,6 @@ terraform {
     }
   }
   backend "azurerm" {
-    resource_group_name  = "StorageAccount-ResourceGroup"
     storage_account_name = "abcd1234"
     container_name       = "tfstate"
     key                  = "training.terraform.tfstate"
