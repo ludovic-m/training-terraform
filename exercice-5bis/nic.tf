e2f1c7ccfe24273b7f64b9290c1f6f97bf772196
Ludovic Médard <ludovic.medard@live.com>
2021-12-01 15:43:09 +0100

    Update nic.tf

--- exercice-5bis/nic.tf
+++ exercice-5bis/nic.tf
@@ -1,6 +1,6 @@
 resource "azurerm_network_interface" "nic_training" {
-  count               = var.nb_vm
-  name                = "nic-${terraform.workspace}-training-${count.index + 1}"
+  for_each            = var.vms
+  name                = "nic-${terraform.workspace}-training-${each.value.name}"
   location            = var.location
   resource_group_name = azurerm_resource_group.rg_training.name
 
@@ -8,6 +8,6 @@ resource "azurerm_network_interface" "nic_training" {
     name                          = "ip-config-training"
     subnet_id                     = azurerm_subnet.subnet_training.id
     private_ip_address_allocation = "Static"
-    private_ip_address            = cidrhost(element(azurerm_subnet.subnet_training.address_prefixes, 1), 10 + count.index)
+    private_ip_address            = cidrhost(element(azurerm_subnet.subnet_training.address_prefixes, 1), 10 + each.value.ip_index)
   }
 }
